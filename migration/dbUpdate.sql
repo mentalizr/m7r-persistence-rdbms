c6e45beb91dc09a5566b31333b66f98bae928146
Fabian Kalk <fabian.kalk@comrize.de>
2023-03-09 13:53:14 +0100

    Rollback function and update on existing SQL files.

--- migration/dbUpdate.sql
+++ migration/dbUpdate.sql
@@ -1,30 +1,24 @@
 /*update existing tables*/
-ALTER TABLE user_login ADD secondFA boolean;
-ALTER TABLE user_login ADD emailConfirmation long;
-ALTER TABLE user_login ADD emailConfToken varchar(255);
-ALTER TABLE user_login ADD emailConfCode varchar(255);
-ALTER TABLE user_login ADD renewPasswordRequired boolean;
+ALTER TABLE user_login ADD second_fa TINYINT(1);
+ALTER TABLE user_login ADD email_confirmation BIGINT;
+ALTER TABLE user_login ADD email_conf_token varchar(255);
+ALTER TABLE user_login ADD email_conf_code varchar(255);
+ALTER TABLE user_login ADD renew_pw_req TINYINT(1);
+ALTER TABLE user_login ADD CONSTRAINT uk_email_conf_token UNIQUE(email_conf_token);
 
 /*update the new columns with a value*/
-UPDATE mentalizr.user_login t SET t.secondFA=0 WHERE t.secondFA IS NULL;
-UPDATE mentalizr.user_login t SET t.renewPasswordRequired=0 WHERE t.renewPasswordRequired IS NULL;
-UPDATE mentalizr.user_login t SET t.emailConfirmation=0 WHERE t.emailConfirmation IS NULL;
-
-/*show updated table*/
-SELECT * FROM user_login;
+UPDATE user_login t SET t.second_fa=0 WHERE t.second_fa IS NULL;
+UPDATE user_login t SET t.renew_pw_req=0 WHERE t.renew_pw_req IS NULL;
+UPDATE user_login t SET t.email_confirmation=0 WHERE t.email_confirmation IS NULL;
 
 /*create table policy_consent*/
-CREATE TABLE mentalizr.policy_consent
+CREATE TABLE policy_consent
 (
-    userid  varchar(255) ,
+    user_id  varchar(255),
     version varchar(255) ,
-    consent long         , 
-    constraint policy_consent_pk
-        primary key (userid, version),
-    constraint policy_consent_user_null_fk
-        foreign key (userid) references mentalizr.user (id)
+    consent BIGINT       ,
+    constraint pk_user_id_version
+        primary key (user_id, version),
+    constraint fk_policy_consent_user_id
+        foreign key (user_id) references user (id)
 );
-
-/*show created table*/
-SELECT * FROM policy_consent;
-
