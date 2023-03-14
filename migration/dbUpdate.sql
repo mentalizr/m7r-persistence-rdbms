47583419ec07ef3c2c5e5b20c9c5338ba5b4f4ee
Fabian Kalk <fabian.kalk@comrize.de>
2023-03-14 14:32:42 +0100

    Adjusted Update to pass all testcases

--- migration/dbUpdate.sql
+++ migration/dbUpdate.sql
@@ -1,24 +1,26 @@
 /*update existing tables*/
-ALTER TABLE user_login ADD second_fa TINYINT(1);
-ALTER TABLE user_login ADD email_confirmation BIGINT;
-ALTER TABLE user_login ADD email_conf_token varchar(255);
-ALTER TABLE user_login ADD email_conf_code varchar(255);
-ALTER TABLE user_login ADD renew_pw_req TINYINT(1);
-ALTER TABLE user_login ADD CONSTRAINT uk_email_conf_token UNIQUE(email_conf_token);
+ALTER TABLE user_login ADD COLUMN second_fa TINYINT(1);
+ALTER TABLE user_login ADD COLUMN email_confirmation BIGINT;
+ALTER TABLE user_login ADD COLUMN email_conf_token varchar(255);
+ALTER TABLE user_login ADD COLUMN email_conf_code varchar(255);
+ALTER TABLE user_login ADD COLUMN renew_pw_req TINYINT(1);
+ALTER TABLE user_login ADD UNIQUE KEY uk_email_conf_token (email_conf_token);
 
 /*update the new columns with a value*/
 UPDATE user_login t SET t.second_fa=0 WHERE t.second_fa IS NULL;
 UPDATE user_login t SET t.renew_pw_req=0 WHERE t.renew_pw_req IS NULL;
 UPDATE user_login t SET t.email_confirmation=0 WHERE t.email_confirmation IS NULL;
 
+/*update user table*/
+ALTER TABLE user MODIFY firstActive BIGINT(20);
+ALTER TABLE user MODIFY lastActive BIGINT(20);
+
 /*create table policy_consent*/
-CREATE TABLE policy_consent
-(
-    user_id  varchar(255),
-    version varchar(255) ,
-    consent BIGINT       ,
-    constraint pk_user_id_version
-        primary key (user_id, version),
-    constraint fk_policy_consent_user_id
-        foreign key (user_id) references user (id)
-);
+CREATE TABLE policy_consent (tempBarnacleGenerator varchar(1));
+ALTER TABLE policy_consent ADD COLUMN (user_id VARCHAR(255));
+ALTER TABLE policy_consent ADD COLUMN (version VARCHAR(255));
+ALTER TABLE policy_consent ADD COLUMN (consent BIGINT);
+ALTER TABLE policy_consent DROP COLUMN tempBarnacleGenerator;
+ALTER TABLE policy_consent ADD PRIMARY KEY (user_id, version);
+ALTER TABLE policy_consent CHARACTER SET UTF8;
+ALTER TABLE policy_consent ADD CONSTRAINT fk_policy_consent_user_id FOREIGN KEY (user_id) REFERENCES user (id);
