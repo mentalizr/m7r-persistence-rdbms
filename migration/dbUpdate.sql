fce70d7560f85d83c4acc63d3e3def156404613c
Arthur Picht <picht@arthurpicht.de>
2023-08-29 17:56:39 +0200

    creation attribute added

--- migration/dbUpdate.sql
+++ migration/dbUpdate.sql
@@ -1,10 +1,10 @@
 /*update existing tables*/
-ALTER TABLE user_login ADD COLUMN second_fa TINYINT(1);
-ALTER TABLE user_login ADD COLUMN email_confirmation BIGINT;
-ALTER TABLE user_login ADD COLUMN email_conf_token varchar(255);
-ALTER TABLE user_login ADD COLUMN email_conf_code varchar(255);
-ALTER TABLE user_login ADD COLUMN renew_pw_req TINYINT(1);
-ALTER TABLE user_login ADD UNIQUE KEY uk_email_conf_token (email_conf_token);
+ALTER TABLE user_login ADD COLUMN (second_fa TINYINT(1));
+ALTER TABLE user_login ADD COLUMN (email_confirmation BIGINT);
+ALTER TABLE user_login ADD COLUMN (email_conf_token varchar(255));
+ALTER TABLE user_login ADD COLUMN (email_conf_code varchar(255));
+ALTER TABLE user_login ADD COLUMN (renew_pw_req TINYINT(1));
+ALTER TABLE user_login ADD CONSTRAINT uk_email_conf_token UNIQUE (email_conf_token);
 
 /*update the new columns with a value*/
 UPDATE user_login t SET t.second_fa=0 WHERE t.second_fa IS NULL;
@@ -12,8 +12,10 @@ UPDATE user_login t SET t.renew_pw_req=0 WHERE t.renew_pw_req IS NULL;
 UPDATE user_login t SET t.email_confirmation=0 WHERE t.email_confirmation IS NULL;
 
 /*update user table*/
-ALTER TABLE user MODIFY firstActive BIGINT(20);
-ALTER TABLE user MODIFY lastActive BIGINT(20);
+ALTER TABLE user ADD COLUMN (creation BIGINT);
+ALTER TABLE user MODIFY firstActive BIGINT;
+ALTER TABLE user MODIFY lastActive BIGINT;
+UPDATE user SET creation=0;
 
 /*create table policy_consent*/
 CREATE TABLE policy_consent (tempBarnacleGenerator varchar(1));
